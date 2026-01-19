9bcef18ddd6904b99c334d7551d524aff11b937a
Chandrima Ganguly <hello@chandrimaganguly.com>
2026-01-19 09:40:39 +0000

    Revert custom domain configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://chandrimaganguly.com',
+  site: 'https://chandrimaganguly.github.io',
+  base: '/AI_consultant_website',
   integrations: [tailwind()],
   output: 'static',
 });
